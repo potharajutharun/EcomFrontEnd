f8aeeb91ee436ce0c18a146cdb609428599801d1
THARUN RANA <potharajutharunrana@gmail.com>
2024-07-09 16:14:34 +0530

    initial commit

--- src/collections/WishList.js
+++ src/collections/WishList.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Card, Button } from 'react-bootstrap';
-import { IoMdStar } from "react-icons/io";
+
 import { MdDeleteForever } from "react-icons/md";
 import { remove } from '../store/wishlistSlice.js';
 import { add } from '../store/cartSlice.js';
